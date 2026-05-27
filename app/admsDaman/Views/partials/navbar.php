745b4867381e6ee9c4d0cb6904b93461c9c55bcd
Emanoel <emanoel.c.duarte@hotmail.com>
2026-05-27 09:46:29 -0300

    Inclusão da imagem da marca

--- app/admsDaman/Views/partials/navbar.php
+++ app/admsDaman/Views/partials/navbar.php
@@ -1,10 +1,14 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-nav">
     <?php // Navbar Brand 
     ?>
-    <a class="navbar-brand ps-3" href="<?= $_ENV['URL_ADM'] ?>dashboard"><?= $_ENV['APP_NAME'] ?></a>
+    <a class="navbar-brand ps-3" href="<?= $_ENV['URL_ADM'] ?>dashboard">
+        <img src="<?= $_ENV['URL_ADM'] ?>public/admsDaman/image/brand/daman_logo_150px.png"
+            alt="<?= $_ENV['APP_NAME'] ?>" height="40">
+    </a>
     <?php // Sidebar Toggle 
     ?>
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+            class="fas fa-bars"></i></button>
     <?php  // Navbar Search 
     ?>
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
@@ -14,9 +18,11 @@
     ?>
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="<?= $_ENV['URL_ADM'] . 'view-profile/' .$_SESSION['user_id'] ?>">Perfil</a></li>
+                <li><a class="dropdown-item"
+                        href="<?= $_ENV['URL_ADM'] . 'view-profile/' . $_SESSION['user_id'] ?>">Perfil</a></li>
                 <li>
                     <hr class="dropdown-divider" />
                 </li>
